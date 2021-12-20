4f6be7a0ece58850370cef6b8f225dda90fa730c
Lucia Urrea <marlube78@gmail.com>
2021-12-20 00:26:16 -0500

    :sparkles:  handle body request in isMutant function

---
+++ src/controllers/index.js
@@ -0,0 +1,11 @@
+import { validateMutant } from "../utils";
+
+export const isMutant = async(req, res) => {
+    console.log(req);
+    try {
+        const validateIsMutant = await validateMutant(req.body.dna);
+        res.status(200).json(validateIsMutant);
+    } catch (error) {
+        res.status(404).send({ error: error.message });
+    }
+};
