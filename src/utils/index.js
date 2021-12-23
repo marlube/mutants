b41a27e3e624065c01b183f63c82009586eda5e1
Lucia Urrea <marlube78@gmail.com>
2021-12-23 00:11:38 -0500

    :sparkles:  row and column validate function added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,25 +1,53 @@
 export const validateMutant = (dna) => {
     if (!dna) return [];
+    rowCoincidence > 0 ? rowCoincidence = 0 : null;
     const DNA = dna.map((field) => Array.from(field));
-    const data = validateEqualValuesOnRow(DNA);
-    rowValidation(DNA);
-    //console.log(DNA, " Este es DNA");
-    return data;
+    const isMutant = validateStringMutantDNA(DNA);
+    return isMutant;
 };
 
 //count of coincidence, if the coincidence is more than one return true
 let rowCoincidence = 0;
 
+//Validation equal values on row 
 const validateEqualValuesOnRow = (list) =>
     list.length >= 4 && list.some((current, index) => {
         const isValid = index > 1 && current === list[index - 3] && current === list[index - 2] && current === list[index - 1]
         if (isValid) rowCoincidence += 1;
         return isValid;
-    })
+    });
 
 
 // row Validation
-const rowValidation = (dna) => dna.forEach((row) => {
-    console.log(validateEqualValuesOnRow(row));
+const rowValidation = (matriz) => matriz.forEach((row) => {
     return validateEqualValuesOnRow(row);
 });
+
+//transpose columns
+const transposeColumnsToRows = (matriz) => {
+    console.log("matriz tranpose ", matriz);
+    let [row] = matriz;
+    console.log("matriz row ", row);
+    return row.map((_, index) => matriz.map(row => row[index]));
+};
+
+/**
+ * 
+ * @param {*} matriz 
+ */
+const columnValidation = (matriz) => {
+    let columns = transposeColumnsToRows(matriz);
+    console.log("columns ", columns);
+    columns.forEach((column) => validateEqualValuesOnRow(column));
+}
+
+/**
+ * Function that call the all validations
+ * @param {*} dna The DNA string to validate
+ * @returns Object that contains if a DNA is mutant or not and coincidences of the mutant strings
+ */
+const validateStringMutantDNA = (dna) => {
+    rowValidation(dna);
+    columnValidation(dna);
+    return { isMutant: rowCoincidence >= 2, rowCoincidence };
+}
