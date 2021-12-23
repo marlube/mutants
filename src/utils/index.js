20d1b6a16b3935fe99f6072749b73c66d1115607
Lucia Urrea <marlube78@gmail.com>
2021-12-23 14:34:59 -0500

    :sparkles:  function validate diagonals is added

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,7 +23,11 @@ const rowValidation = (matriz) => matriz.forEach((row) => {
     return validateEqualValuesOnRow(row);
 });
 
-//transpose columns
+/**
+ * 
+ * @param {*} matriz 
+ * @returns 
+ */
 const transposeColumnsToRows = (matriz) => {
     console.log("matriz tranpose ", matriz);
     let [row] = matriz;
@@ -32,14 +36,35 @@ const transposeColumnsToRows = (matriz) => {
 };
 
 /**
- * 
- * @param {*} matriz 
+ * Function that validate if the columns have a string mutant
+ * @param {*} matriz the DNA string to validate
  */
 const columnValidation = (matriz) => {
     let columns = transposeColumnsToRows(matriz);
     console.log("columns ", columns);
     columns.forEach((column) => validateEqualValuesOnRow(column));
 }
+
+//diagonal validation
+const trasposeDiagonalToList = (matrix) => {
+    const matrixArea = matrix[0].length;
+    if (matrixArea < 4) return [];
+
+    const diagonalIndex = new Array(matrixArea).fill("").map((_, matrixIndex) => matrix.map((row, index) => row[index + matrixIndex]).filter(row => !!row));
+
+
+    const matrixColumnsToRows = transposeColumnsToRows(matrix);
+
+    const inverseDiagonalIndex = new Array(matrixArea).fill("").map((_, matrixIndex) => matrixColumnsToRows.map((row, index) => row[index + matrixIndex]).filter(row => !!row)).filter((_, index) => index > 0);
+
+    return [...diagonalIndex, ...inverseDiagonalIndex];
+};
+
+//Diagonal Validation
+const diagonalValidate = (matriz) => {
+    const diagonals = trasposeDiagonalToList(matriz);
+    diagonals.forEach((diagonal) => validateEqualValuesOnRow(diagonal));
+};
 
 /**
  * Function that call the all validations
@@ -49,5 +74,6 @@ const columnValidation = (matriz) => {
 const validateStringMutantDNA = (dna) => {
     rowValidation(dna);
     columnValidation(dna);
+    diagonalValidate(dna)
     return { isMutant: rowCoincidence >= 2, rowCoincidence };
-}
+};
