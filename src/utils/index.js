db05c92537c33ea356846b7e674eda31bb507af0
marlube <marlube78@gmail.com>
2021-12-28 04:22:35 -0500

    :sparkles:  added function for validate errors

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,14 +2,21 @@ export const validateMutant = (dna) => {
     if (!dna) return [];
     rowCoincidence > 0 ? rowCoincidence = 0 : null;
     const DNA = dna.map((field) => Array.from(field));
+    validateDimentionMatriz(DNA);
     const isMutant = validateStringMutantDNA(DNA);
     return isMutant;
 };
 
-//count of coincidence, if the coincidence is more than one return true
+/**
+ * count of coincidence, if the coincidence is more than one return true
+ */
 let rowCoincidence = 0;
 
-//Validation equal values on row 
+/**
+ * Function that validate if exist equal values on row 
+ * @param {*} list the DNA string to validate
+ * @returns true or false 
+ */
 const validateEqualValuesOnRow = (list) =>
     list.length >= 4 && list.some((current, index) => {
         const isValid = index > 1 && current === list[index - 3] && current === list[index - 2] && current === list[index - 1]
@@ -17,21 +24,46 @@ const validateEqualValuesOnRow = (list) =>
         return isValid;
     });
 
+/**
+ * Function that validate if have caracters diferents 
+ * @param {*} list the DNA string to validate
+ * @returns Error 400 bat request
+ */
+const validateDnaInMatriz = (list) =>
+    list.forEach((current) => {
+        if (current !== "A" && current !== "T" && current !== "C" && current !== "G") {
+            throw (new SyntaxError('Hay un caracter diferente a la cadena de DNA ingrese un valor valido'));
+        }
+    });
 
-// row Validation
+/**
+ * Function that validate dimention of the matriz NxN
+ * @param {*} matriz the DNA string to validate
+ * @returns Error 400 bat request
+ */
+const validateDimentionMatriz = (matriz) => matriz.map((_, index) => {
+    if (matriz.length !== matriz[index].length) {
+        throw (new SyntaxError('La cadena de DNA no tiene todos los valores'));
+    }
+});
+
+/**
+ * Function that validate row 
+ * @param {*} matriz the DNA string to validate
+ * @returns Validated rows 
+ */
 const rowValidation = (matriz) => matriz.forEach((row) => {
+    validateDnaInMatriz(row);
     return validateEqualValuesOnRow(row);
 });
 
 /**
- * 
- * @param {*} matriz 
- * @returns 
+ * Function that transpose columns to rows 
+ * @param {*} matriz the DNA string to validate
+ * @returns the matriz transpose 
  */
 const transposeColumnsToRows = (matriz) => {
-    console.log("matriz tranpose ", matriz);
     let [row] = matriz;
-    console.log("matriz row ", row);
     return row.map((_, index) => matriz.map(row => row[index]));
 };
 
@@ -41,17 +73,19 @@ const transposeColumnsToRows = (matriz) => {
  */
 const columnValidation = (matriz) => {
     let columns = transposeColumnsToRows(matriz);
-    console.log("columns ", columns);
     columns.forEach((column) => validateEqualValuesOnRow(column));
 }
 
-//diagonal validation
+/**
+ * Function that transpose diagonals to rows for validate digonals like rows
+ * @param {*} matrix The DNA matriz to validate
+ * @returns The diagonals like a rows 
+ */
 const trasposeDiagonalToList = (matrix) => {
     const matrixArea = matrix[0].length;
     if (matrixArea < 4) return [];
 
     const diagonalIndex = new Array(matrixArea).fill("").map((_, matrixIndex) => matrix.map((row, index) => row[index + matrixIndex]).filter(row => !!row));
-
 
     const matrixColumnsToRows = transposeColumnsToRows(matrix);
 
@@ -60,7 +94,10 @@ const trasposeDiagonalToList = (matrix) => {
     return [...diagonalIndex, ...inverseDiagonalIndex];
 };
 
-//Diagonal Validation
+/** 
+ * Funcion that validate diagonal  
+ * @param {*} matriz The DNA matriz to validate
+ */
 const diagonalValidate = (matriz) => {
     const diagonals = trasposeDiagonalToList(matriz);
     diagonals.forEach((diagonal) => validateEqualValuesOnRow(diagonal));
