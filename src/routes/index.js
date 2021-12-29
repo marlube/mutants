8635f49692bc059ed81900f7bf7e9a5060c41c9c
marlube <marlube78@gmail.com>
2021-12-28 22:45:26 -0500

    :truck:  endpoint get route is created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { isMutant } from "../controllers";
+import { countDnaValidate } from "../validators";
 
 const router = Router();
 
 router.post("/mutant/", isMutant);
+router.get("/stats", countDnaValidate);
 
 export default router;
