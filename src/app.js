5093a1e82423776cc7bc46603790f4d115c85fd5
marlube <marlube78@gmail.com>
2021-12-29 00:04:39 -0500

    :rocket:  dynamic port added

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ const cors = require("cors");
 
 // created express app
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(indexRoutes);
-app.listen(3000, () => {
-    console.log('Escuchando en puerto 3000');
+app.listen(PORT, () => {
+    console.log(`Escuchando en puerto ${PORT}`);
 });
