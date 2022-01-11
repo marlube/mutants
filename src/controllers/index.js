b1ce81afa5c63dd88acded1297231db890b68bc3
marlube <marlube78@gmail.com>
2022-01-10 20:02:36 -0500

    :sparkles: function getstats added

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 import { validateMutant } from "../utils";
 import { db } from "../db";
+import { countDnaValidate } from "../validators"
 
 /**
  * Function that validate errors 
@@ -11,8 +12,34 @@ export const isMutant = async(req, res) => {
         const validateIsMutant = await validateMutant(req.body.dna);
         const { isMutant } = validateIsMutant;
         const isNotMutantMessage = 'El DNA enviado pertenece a un humano';
-        db.ref('dnaValidate').push(req.body.dna);
+        const newDnaValidation = {
+            dna: req.body.dna,
+            isMutant
+        };
+        db.ref('dnaValidate').push(newDnaValidation);
         isMutant ? res.status(200).json(validateIsMutant) : res.status(403).send({ error: isNotMutantMessage });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+};
+
+export const getStats = async(req, res) => {
+    try {
+        let statics;
+        // Human and Mutant counters
+        let countHuman = 0;
+        let countMutant = 0;
+        db.ref('dnaValidate').once('value', (snapshot) => {
+            const data = snapshot.val();
+            for (const key in data) {
+                if (Object.hasOwnProperty.call(data, key)) {
+                    const element = data[key];
+                    element.isMutant ? countMutant += 1 : countHuman += 1;
+                    statics = countDnaValidate(countHuman, countMutant);
+                }
+            }
+            res.status(200).json(statics);
+        });
     } catch (error) {
         res.status(400).send({ error: error.message });
     }
